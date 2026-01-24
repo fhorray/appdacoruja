9bcc7c21802e99a89b191c35d726b60a67fdab31
Francy Santos <francy.horray@gmail.com>
2026-01-24 17:44:41 -0300

    :tada: fiorst commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { UserButton } from '@/components/auth/user-button';
+
+export default function Home() {
+  return (
+    <div>
+      <UserButton />
+      Welcome!
+    </div>
+  );
+}
